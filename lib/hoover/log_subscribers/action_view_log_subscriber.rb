4e6be1e770e120dcffc0ce42e738557c4802c99f
Gabe da Silveira <gabe@websaviour.com>
2011-10-26 01:03:41 -0600

    Don't need ActionView data in loggly
    If someone needs it I'm open to suggestions though

--- lib/hoover/log_subscribers/action_view_log_subscriber.rb
+++ lib/hoover/log_subscribers/action_view_log_subscriber.rb
@@ -1,19 +1,5 @@
 module Hoover
   class ActionViewLogSubscriber < ActiveSupport::LogSubscriber
-    def render_template(event)
-      rendered = { :identifier => from_rails_root(event.payload[:identifier]),
-                   :duration => event.duration }
-      rendered[:layout] = from_rails_root(event.payload[:layout]) if event.payload[:layout]
-
-      Hoover.add :rendered => rendered
-    end
-    alias :render_partial :render_template
-    alias :render_collection :render_template
-
-  protected
-
-    def from_rails_root(string)
-      string.sub("#{Rails.root}/", "").sub(/^app\/views\//, "")
-    end
+    # I have no need to log this to Loggly, implementations welcome.
   end
 end
